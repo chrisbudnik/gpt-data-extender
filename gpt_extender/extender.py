609376d303c72bd34c1e68c3075c7f08ef5bc664
chrisb <chrism.budnik@gmail.com>
2023-09-14 00:44:00 +0200

    update: add doc strings

--- gpt_extender/extender.py
+++ gpt_extender/extender.py
@@ -8,6 +8,7 @@ from .templates import ExtendTemplate
 
 
 class DataExtender:
+    """Extends a given DataFrame using various AI-powered methods."""
 
     def __init__(self, 
                  df: pd.DataFrame, 
@@ -15,7 +16,8 @@ class DataExtender:
                  chat_model: str = "gpt-3.5-turbo",
                  embeddings_model: str = "text-embedding-ada-002"
                  ) -> None:
-        
+        """Initialize the DataExtender class with specific AI models."""
+
         self.df = df
         self.gpt_model = gpt_model
         self.chat_model = chat_model
@@ -25,6 +27,8 @@ class DataExtender:
         self.completion_tokens = 0
 
     def _chat_completion(self, context: str, prompt: str, **kwargs):
+        """Get completion for chat-based models."""
+
         response = openai.ChatCompletion.create(
             model=self.chat_model,
             messages=[
@@ -37,6 +41,8 @@ class DataExtender:
         return self._process_chat_response(response)
 
     def _gpt_completion(self, prompt: str, **kwargs):
+        """Get completion for GPT-based models."""
+
         response = openai.Completion.create(
             engine=self.gpt_model,
             prompt=prompt,
@@ -47,18 +53,26 @@ class DataExtender:
 
     @staticmethod
     def _process_chat_response(res: OpenAIObject) -> str:
+        """Process and extract content from chat response."""
+
         response_dict = res.to_dict_recursive()
         return response_dict.get("choices", {})[0].get("message", {}).get("content", None)
 
     def _update_token_usage(self, res: OpenAIObject) -> None:
+        """Update token usage for API calls."""
+
         self.prompt_tokens += res.get("usage", {}).get("prompt_tokens", 0)
         self.completion_tokens += res.get("usage", {}).get("completion_tokens", 0)
 
     def sample_to_text(self, column_name: str, sample_size: int = 5) -> str:
+        """Return a text sample from a specific DataFrame column."""
+
         sample = self.df[column_name].sample(sample_size)
         return "\n".join(sample)
     
     def chat_extend(self, template: ExtendTemplate) -> pd.DataFrame:
+        """Extend DataFrame using a chat-based model."""
+
         generated_data = []
         for index, row in self.df.iterrows():
             data = row[template.column_name] 
@@ -69,6 +83,8 @@ class DataExtender:
         return self.df
 
     def gpt_extend(self, template: ExtendTemplate) -> pd.DataFrame:
+        """Extend DataFrame using a GPT-based model."""
+
         generated_data = []
 
         for index, row in self.df.iterrows():
@@ -87,6 +103,8 @@ class DataExtender:
                          inplace: bool = True,
                          flag_synthetic_data: bool = False
                          ) -> pd.DataFrame:
+        """Generate and extend DataFrame with synthetic data."""
+
 
         sample_records = self.sample_to_text(template.column_name, sample_size)
         
@@ -109,9 +127,13 @@ class DataExtender:
         return extended_df
     
     def add_embeddings(self, column_name: str):
+        """Add embeddings for a specific DataFrame column."""
+
         pass
 
     def add_sentiment(self, column_name: str, new_column_name: str, outputs: list[str]):
+        """Add sentiment analysis to a specific DataFrame column."""
+
 
         template = ExtendTemplate(column_name=column_name,
                                   new_column_name=new_column_name,
@@ -123,6 +145,8 @@ class DataExtender:
         return self.chat_extend(template=template)
 
     def add_translation(self, column_name: str, language: str):
+        """Add translation to a specific DataFrame column."""
+
         new_column_name = column_name + "_" + language
         template = ExtendTemplate(column_name=column_name,
                                   new_column_name=new_column_name,
@@ -133,6 +157,8 @@ class DataExtender:
         return self.chat_extend(template=template)
 
     def add_summary(self, column_name: str, new_column_name: str):
+        """Add text summary to a specific DataFrame column."""
+
         template = ExtendTemplate(column_name=column_name,
                                   new_column_name=new_column_name,
                                   context="You are careful reader and researcher who does not make things up.",
@@ -142,6 +168,8 @@ class DataExtender:
         return self.chat_extend(template=template)
 
     def add_topic(self, column_name: str, new_column_name: str, outputs: list[str]):
+        """Add topic labels to a specific DataFrame column."""
+
         template = ExtendTemplate(column_name=column_name,
                                   new_column_name=new_column_name,
                                   context="You are careful reader and researcher who does not make things up.",
@@ -152,6 +180,8 @@ class DataExtender:
         return self.chat_extend(template=template)
     
     def add_synthetic_data(self, column_name: str, **kwargs):
+        """Add synthetic data to the DataFrame."""
+
         template = ExtendTemplate(column_name=column_name,
                                   new_column_name="",
                                   context="You are an expert data specialist capable of understanding and learing patterns from data.",
@@ -161,6 +191,8 @@ class DataExtender:
         return self.synthetic_extend(template=template, **kwargs)
     
     def usage_summary(self, cost_1k_tokens=0.002) -> dict[str: int]:
+        """Get summary of API token usage and estimated cost."""
+
         usage_dict = {
             "prompt_tokens": self.prompt_tokens,
             "completion_tokens": self.completion_tokens,
@@ -170,6 +202,8 @@ class DataExtender:
         return usage_dict
     
     def ai_analyze(self, column_name: str, **kwargs) -> None:
+        """Perform AI-based analysis to suggest DataFrame extensions."""
+
         sample = self.sample_to_text(column_name, **kwargs)
         template = ExtendTemplate(column_name=column_name,
                                   new_column_name="",
